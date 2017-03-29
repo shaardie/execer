3aaf9bc0a9090beef630dd7a7b2804a4f53524c7
Sven Haardiek <sven.haardiek@greenbone.net>
2017-03-29 17:26:40 +0200

    Reasonable example

--- doc_test.go
+++ doc_test.go
@@ -1,9 +1,28 @@
 package execer
 
+import "fmt"
+
 func Example() {
+	// Create the execer
 	cmd := []string{"echo", "Hello World!"}
-	_, err := Init(cmd)
+	execer, err := Init(cmd)
 	if err != nil {
-		// Something went wrong during creation
+		fmt.Println(err)
+		return
 	}
+
+	// Start the command in the background
+	err = execer.Start()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Get status of the command
+	status := execer.Status()
+	if status.Error != nil {
+		fmt.Println(status.Error)
+		return
+	}
+	fmt.Println(status.Stdout)
 }
