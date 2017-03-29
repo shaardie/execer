366de4adccc8b3294a56deffac38b20616521f08
Sven Haardiek <sven.haardiek@greenbone.net>
2017-03-29 17:07:47 +0200

    prettify doc

--- execer.go
+++ execer.go
@@ -19,13 +19,13 @@ type Execer struct {
 	stderr   string
 }
 
-// Status represent the (temporarily) status of a `Execer`.
+// Status represent the (temporarily) status of a Execer.
 //
-// `Error` stores the (last) error.
-// `Finished` indicates if the command still running.
-// `Started` indicates if the command is already started.
-// `Stdout` contains the commands stdout until the time the `Status` was created.
-// `Stderr` contains the commands stderr until the time the `Status` was created.
+// Error stores the (last) error.
+// Finished indicates if the command still running.
+// Started indicates if the command is already started.
+// Stdout contains the commands stdout until the time the Status was created.
+// Stderr contains the commands stderr until the time the Status was created.
 type Status struct {
 	Error    error
 	Finished bool
@@ -34,7 +34,9 @@ type Status struct {
 	Stderr   string
 }
 
-// Create a new `Execer`. `len(cmd) == 0` is not allowed.
+// Create a new Execer.
+//  len(cmd) == 0
+// is not allowed.
 func Init(cmd []string) (Execer, error) {
 	e := Execer{}
 	if len(cmd) == 0 {
